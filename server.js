952431223f351624c596d58ab089ffa23c58a339
ntamauwe-cmyk <ntama.uwe@gmail.com>
2025-10-07 18:21:43 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect Database
+connectDB();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(express.static('views'));
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
