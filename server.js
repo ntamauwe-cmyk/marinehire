de597eeaab222d7aa1c8dfd4a2c16b49627fd055
ntamauwe-cmyk <ntama.uwe@gmail.com>
2025-10-08 19:48:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const connectDB = require('./db');
-const authRoutes = require('./auth');
-
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth');
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 // Connect Database
@@ -15,5 +15,5 @@ app.use(express.static('views'));
 
 // Routes
 app.use('/api/auth', authRoutes);
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// start server
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
