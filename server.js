5e53ad249a1447ac87224eac1df5cceb790738c7
ntamauwe-cmyk <ntama.uwe@gmail.com>
2025-10-09 18:04:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,18 +4,22 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 
-dotenv.config();
+dotenv.config(); // Load environment variables first
 
-const app = express();
+const app = express(); // initiate Express
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use('/api/auth', authRoutes);
 
+// Connect to MongoDB
 connectDB();
 
+// Start server
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
-  console.log(Server running on port ${PORT});
+  console.log(`Server running on port ${PORT}`);
 });
