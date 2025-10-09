bc48cdc3591a7b9001c8006381df64a130e97abb
ntamauwe-cmyk <ntama.uwe@gmail.com>
2025-10-09 13:04:11 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,16 +5,14 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DATABASE_URL, 
-{
+    const conn = await mongoose.connect(process.env.DATABASE_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`MongoDB connected: $
-      {conn.connection.host}`);
+
+    console.log(✅ MongoDB connected: ${conn.connection.host});
   } catch (error) {
-    console.error(`Error: ${error.message}
-    `);
+    console.error(❌ Error: ${error.message});
     process.exit(1);
   }
 };
