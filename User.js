415c6c472073d9790728c136e29731f127807e3a
ntamauwe-cmyk <ntama.uwe@gmail.com>
2025-10-09 16:33:35 +0100

    Update User.js

--- User.js
+++ User.js
@@ -1,29 +1,41 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, 'Please enter your name'],
-    },
-    email: {
-      type: String,
-      required: [true, 'Please enter your email'],
-      unique: true,
-      match: [
-        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-        'Please enter a valid email address',
-      ],
-    },
-    password: {
-      type: String,
-      required: [true, 'Please enter your password'],
-      minlength: [6, 'Password must be at least 6 characters'],
-    },
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
   },
-  { timestamps: true }
-);
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    default: 'user' // can be 'admin', 'user', etc.
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Pre-save hook to hash password
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+export default User;
