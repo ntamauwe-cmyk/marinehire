42256c4362ad2a788d60015b355ef917e8a78756
ntamauwe-cmyk <ntama.uwe@gmail.com>
2025-10-11 17:14:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-import authRoutes from './routes/authRoutes.js';
+import authRoutes from './routes/auth.js';
+
 
 dotenv.config(); // Load environment variables first
 
