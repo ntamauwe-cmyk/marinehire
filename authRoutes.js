56dd217f9d95614ab1ec20eae04a4d8edd427011
ntamauwe-cmyk <ntama.uwe@gmail.com>
2025-10-18 18:20:29 +0100

    Update comments for auth routes

--- authRoutes.js
+++ authRoutes.js
@@ -1,15 +1,17 @@
 // authRoutes.js
 import express from "express";
-import { registerUser, loginUser } from "./authController.js"; // note the ./authController.js path
+import { registerUser, loginUser } from "./authController.js";
 
 const router = express.Router();
 
 // @route   POST /api/auth/register
 // @desc    Register a new user
+// @access  Public
 router.post("/register", registerUser);
 
 // @route   POST /api/auth/login
-// @desc    Log in an existing user
+// @desc    Log in a user
+// @access  Public
 router.post("/login", loginUser);
 
 export default router;
